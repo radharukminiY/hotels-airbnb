aece755b801ba710471f9020aaf3be8ca46fc186
Radha YARAMALA <45042325+radharukminiY@users.noreply.github.com>
2021-09-20 12:34:59 +0530

    commit 5

---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Footer.css';
+import { Button } from '@material-ui/core';
+
+function Footer() {
+    return (
+        <div className='footer'>
+            <p>Cloned by:RR</p>
+            <br />
+            <p>Made using MERN Stack with ❤️ </p>
+            <br />
+            <p>© 2021 AirBnB!!! </p>
+        </div>
+    )
+}
+export default Footer
